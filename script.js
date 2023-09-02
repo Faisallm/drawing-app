7cc8b6d54fe424f2108cd0146f87c350a80b7694
Faisallm <faisallawan1997@gmail.com>
2023-09-02 14:08:40 -0700

    Done with the rectangle tool

--- script.js
+++ script.js
@@ -4,48 +4,79 @@ const canvas = document.querySelector("canvas");
 ctx = canvas.getContext("2d");
 // select all option with tool class
 toolbtns = document.querySelectorAll(".tool");
+fillColor = document.querySelector("#fill-color");
 
+// global variables with default values
 let isDrawing = false;
+selectedTool = "brush"; // default
 brushWidth = 5;
+let prevMouseX, prevMouseY, snapshot;
 
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
 
-const startDraw = () => {
-    isDrawing = true;
-    // begin a new path of drawing
-    ctx.beginPath();
-}
+const startDraw = (e) => {
+  isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
+  // begin a new path of drawing
+  ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+const drawRect = (e) => {
+  if (!fillColor.checked) {
+    return ctx.strokeRect(
+      e.offsetX,
+      e.offsetY,
+      prevMouseX - e.offsetX,
+      prevMouseY - e.offsetY
+    );
+  }
+
+  ctx.fillRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
+};
 
 const drawing = (e) => {
   if (!isDrawing) return;
-    // create line according to mouse pointer
+  ctx.putImageData(snapshot, 0, 0);
+  // create line according to mouse pointer
+  if (selectedTool == "brush") {
     ctx.lineTo(e.offsetX, e.offsetY);
     // filling/drawing line with color
     ctx.stroke();
-    ctx.lineWidth = brushWidth;
+  } else if (selectedTool == "rectangle") {
+    drawRect(e);
+  }
 };
 
 // add an event listener to each button
 // this click event will return the
 // id of the particular btn that was clicked
-toolbtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        // remove the default active btn or other no
-        // longer active btn
-        document.querySelector('.options .active').classList.remove('active');
-        btn.classList.add('active');
-        console.log(btn.id);
-    })
-})
-
+toolbtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // remove the default active btn or other no
+    // longer active btn
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    // changing the selected tool variable
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
 
 // the canvas should listen for movement of the mouse
 // and call function drawing
 
 // also the arrow?
 canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mousemove", drawing);
